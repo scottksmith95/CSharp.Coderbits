d2380859162cec914f62e552447beedaa6ce40e1
Scott Smith <scottksmith95@gmail.com>
2013-05-22 07:30:34 -0700

    Updated dependencies to latest version

--- src/CSharp.Coderbits/Properties/AssemblyInfo.cs
+++ src/CSharp.Coderbits/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("C4A56898-A920-4F08-B0A6-8E39FF9EF743")]
 
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
